a2f61138f17fbb4edde3dd5ad3f37f57b52087bd
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-05-15 20:05:46 +0530

    5/15

---
+++ 82_ternary.js
@@ -0,0 +1,4 @@
+//wap to check voter is eligible for voting or not.
+let age;
+age = Number(prompt("enter your age : "))
+age >= 18 ? document.write("eligible for voting") : document.write("not eligible for voting");
