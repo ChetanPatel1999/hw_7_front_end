5db09770ffd73cb5fd089222aefc8423be19068c
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-07-30 21:12:15 +0530

    30/07

--- Currency_converter/app.js
+++ Currency_converter/app.js
@@ -1,4 +1,8 @@
 let dropdowns = document.querySelectorAll(".select-container select");
+let btn = document.querySelector("button");
+let from = document.querySelector(".from select")
+let to = document.querySelector(".to select")
+let msg = document.querySelector(".msg")
 for (let select of dropdowns) {
     for (let curcode in currencyToCountry) {
         let newOption = document.createElement("option");
@@ -19,6 +23,37 @@ for (let select of dropdowns) {
         img.src = `https://flagsapi.com/${currencyToCountry[evt.target.value]}/flat/64.png`
     })
 }
-
+async function exchangeAmount(amountValue) {
+    let url = `https://api.frankfurter.app/latest?amount=1&from=${from.value}&to=${to.value}`;
+    let response = await fetch(url)
+    let data = await response.json();
+    console.log(data.rates[to.value]);
+    let exvalue = data.rates[to.value];
+    let res = amountValue * exvalue;
+    console.log(res);
+    msg.innerText = `${amountValue}${from.value} = ${res}${to.value}`
+}
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+    if (amountValue == "" || amountValue < 1) {
+        amount.value = "1";
+        amountValue = "1";
+    }
+    console.log(amountValue)
+    exchangeAmount(amountValue);
+})
+window.addEventListener("load", (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+    if (amountValue == "" || amountValue < 1) {
+        amount.value = "1";
+        amountValue = "1";
+    }
+    console.log(amountValue)
+    exchangeAmount(amountValue);
+})
 
 
