9a36b1a06d6464fbd65fb490a4a4b2391b6e17c4
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-08-11 20:53:57 +0530

    11/08

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -1,14 +1,16 @@
 let timerDisplay = document.querySelector(".timer-display");
 let start = document.querySelector("#start");
-let stop = document.querySelector("#stop");
+let lap = document.querySelector("#lap");
 let reset = document.querySelector("#reset");
+let body = document.querySelector("body");
 
 let msec = 0;
 let secs = 0;
 let mins = 0;
 
 let timerId = null;
-
+let div = null;
+let id = 0;
 function setTimer() {
     msec++;
     if (msec == 100) {
@@ -31,17 +33,50 @@ start.addEventListener("click", () => {
 
     if (timerId == null) {
         timerId = setInterval(setTimer, 10);
+        start.style.backgroundColor = "red";
+        start.innerText = "Stop";
+        lap.style.visibility = "visible";
+        reset.style.visibility = "visible";
     }
-})
-
-stop.addEventListener("click", () => {
-    clearInterval(timerId);
-    timerId = null;
+    else {
+        clearInterval(timerId);
+        timerId = null;
+        start.style.backgroundColor = "green";
+        start.innerText = "Start";
+        lap.style.visibility = "hidden";
+    }
 })
 reset.addEventListener("click", () => {
     clearInterval(timerId);
     timerId = null;
     msec = secs = mins = 0;
     timerDisplay.innerHTML = "00 : 00 : 00";
-    
+    start.style.backgroundColor = "green";
+    start.innerText = "Start";
+    if (div != null) {
+        div.remove();
+        div = null;
+        id = 0;
+    }
+    lap.style.visibility = "hidden";
+    reset.style.visibility = "hidden";
+})
+lap.addEventListener("click", () => {
+    if (div == null) {
+        div = document.createElement("div");
+        div.id = "timer-lap";
+        let h3 = document.createElement("h3");
+        h3.innerText = "Timer - Lap";
+        let p = document.createElement("p");
+        p.innerText = `#${++id} ${timerDisplay.innerText}`;
+        div.append(h3);
+        div.append(p);
+        body.append(div);
+
+    }
+    else {
+        let p = document.createElement("p");
+        p.innerText = `#${++id} ${timerDisplay.innerText}`;
+        div.append(p);
+    }
 })
