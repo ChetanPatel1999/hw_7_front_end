0fc71ac590b81234868bc34798eddb1252b4fbac
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-10-10 21:12:51 +0530

    9/27

--- Jquery/test.js
+++ Jquery/test.js
@@ -1,1 +1,48 @@
-console.log("hello js");
+// $(document).ready(function () {
+
+//     $("button").click(() => {
+//         //element access by tag name
+//         // $("p").hide();
+
+//         //element access by id name
+//         // $("#para").hide();
+
+//         //element access by class name 
+//         // $(".my-class").hide();
+//         // $(".my-class").css("color", "red");
+//         // $(".my-class").css("background-color", "red");
+
+//     })
+
+// })
+
+
+// $(function () {
+
+//     $("p").click(function () {
+
+//         // $("p").hide();
+
+//         // $("#para").hide();
+
+//         $(this).hide();
+
+//     });
+// });
+
+
+//jquery selecter 
+$(document).ready(function () {
+    $(".btn").click(function () {
+
+        // $("*").hide();
+        // $(this).hide();
+        // $("p.my-class").hide();
+        // $("p:first").hide();
+        // $("p:last").hide();
+        // $("div p:first").hide();
+        $(".box p:first").hide();
+
+    });
+});
+
