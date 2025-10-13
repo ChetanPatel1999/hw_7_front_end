f2c44fdb26d68c5ca28ba0f61b162409c60e511e
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-10-13 21:06:26 +0530

    13/10

--- Jquery/test.js
+++ Jquery/test.js
@@ -32,17 +32,101 @@
 
 
 //jquery selecter 
+// $(document).ready(function () {
+//     $(".btn").click(function () {
+
+//         // $("*").hide();
+//         // $(this).hide();
+//         // $("p.my-class").hide();
+//         // $("p:first").hide();
+//         // $("p:last").hide();
+//         // $("div p:first").hide();
+//         $(".box p:first").hide();
+
+//     });
+// });  
+
+//JQuery events 
 $(document).ready(function () {
-    $(".btn").click(function () {
 
-        // $("*").hide();
-        // $(this).hide();
-        // $("p.my-class").hide();
-        // $("p:first").hide();
-        // $("p:last").hide();
-        // $("div p:first").hide();
-        $(".box p:first").hide();
+    // $("#para").click(function () {
+    //     $(".btn").css("background-color","red");
+    // })
 
-    });
+    //dblclick()
+
+    // $(".btn").dblclick(()=>{
+    //     console.log("dblclick is perform");
+    // })
+
+    //  mouseenter()
+    //  mouseleave()
+
+    // $(".box").mouseenter(function () {
+    //     $("div p").hide();
+    // });
+
+    // $(".box").mouseleave(function () {
+    //     $("div p").show();
+    // });
+
+    //
+    // $(".box").mousedown(function () {
+    //     $("div p").hide();
+    // });
+
+    // $(".box").mouseup(function () {
+    //     $("div p").show();
+    // });
+
+    // //hover
+
+    // $(".box").hover(function () {
+    //     // over
+    //     $(this).css("color", "white");
+    // }, function () {
+    //     // out
+    //     $(this).css("color", "black");
+    // }
+    // );
+
+    // $("input").focus(function () {
+    //     $(this).css("background-color", "red")
+    // });
+
+    // $("input").blur(function () {
+    //     $(this).css("background-color", "white")
+    // });
+
+
+    //The on() Method
+
+    // $(".btn").on("click", function () {
+    //     $(".box").css("background-color", "white")
+    // });
+
+    // $("p").on({
+    //     mouseenter: function () {
+    //         $(this).css("background-color", "lightgray");
+    //     },
+    //     mouseleave: function () {
+    //         $(this).css("background-color", "white");
+    //     },
+    //     click: function () {
+    //         $(this).css("background-color", "yellow");
+    //     }
+    // });
+
+    $(".btn").click(
+        function () {
+            let color = $(".box").css("background-color");
+            if (color === "rgb(255, 0, 0)") {
+                $(".box").css("background-color", "white");
+            }
+            else {
+                $(".box").css("background-color", "red");
+            }
+        }
+    );
 });
 
